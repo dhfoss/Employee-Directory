69a1a9fde8128f34440c93063cf4abac810e0a71
Daniel <dhfoss@gmail.com>
2021-02-04 16:45:21 -0500

    Spaced out buttons

--- employee-directory/src/components/Buttons/index.js
+++ employee-directory/src/components/Buttons/index.js
@@ -3,10 +3,10 @@ import React from 'react';
 const Buttons = ({orderEmployees}) => {
     return (
         <div className="container mb-3">
-            <button className="btn btn-primary" id="lastNameButton" onClick={e => orderEmployees(e)}>Sort by Last Name</button>
-            <button className="btn btn-primary" id="emailButton" onClick={e => orderEmployees(e)}>Sort By Email</button>
-            <button className="btn btn-primary" id="cityButton" onClick={e => orderEmployees(e)}>Sort By City</button>
-            <button className="btn btn-primary" id="countryButton" onClick={e => orderEmployees(e)}>Sort By Country</button>
+            <button className="btn btn-primary mx-3" id="lastNameButton" onClick={e => orderEmployees(e)}>Sort by Last Name</button>
+            <button className="btn btn-primary mx-3" id="emailButton" onClick={e => orderEmployees(e)}>Sort By Email</button>
+            <button className="btn btn-primary mx-3" id="cityButton" onClick={e => orderEmployees(e)}>Sort By City</button>
+            <button className="btn btn-primary mx-3" id="countryButton" onClick={e => orderEmployees(e)}>Sort By Country</button>
         </div>
     )
 }
